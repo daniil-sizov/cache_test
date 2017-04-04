8b7e838eda6078769eb1b9cc5de979b3421e8ad1
Daniil Sizov <daniil.sizov@gmail.com>
2017-04-05 00:59:27 +0700

    Add CacheResult enum

--- src/constants.h
+++ src/constants.h
@@ -12,6 +12,14 @@ enum ReplacementPolicy {
     FIFO
 };
 
+enum CacheResult {
+    MISS                    = 0x0,
+    HIT                     = 0x1,
+    CLEAN_REPLACED          = 0x10,
+    DIRTY_REPLACED          = 0x11,
+    NOTHING_REPLACED        = 0x100
+}
+
 const char * const CONFIG_PATH = "config.json";
 
 #endif // CONSTANTS_H
